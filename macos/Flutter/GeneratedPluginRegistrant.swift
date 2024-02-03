f6307bd482f12b672c3e52d10cc3e94b397751a7
rayzor <rayneville10@gmail.com>
2024-02-03 17:10:33 +0000

    Delete unused library
    Delete unused library 'pckage_info_plus' causing problems in Apple iOS Xcode build.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import package_info_plus
 import path_provider_foundation
 import share_plus
 import sqflite
@@ -13,7 +12,6 @@ import url_launcher_macos
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
